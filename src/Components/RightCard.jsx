e6866af85bf5a5cd652ef5a0fcb245800170e49e
Surendra07Goswami <surendragos27@gmail.com>
2026-01-14 21:06:02 +0530

    change

--- src/Components/RightCard.jsx
+++ src/Components/RightCard.jsx
@@ -3,11 +3,11 @@ const RightCard = () => {
     <div className="w-1/2 h-full bg-gray-800 overflow-x-auto overflow-y-hidden">
       <div className="flex gap-6 py-30 px-20 min-w-max">
         {/* posters */}
-        <img src="/jb.jpg" className="h-80 rounded-lg" />
-        <img src="/taylor.jpg" className="h-80 rounded-lg" />
-        <img src="/camila.jpg" className="h-80 rounded-lg" />
-        <img src="/selena.jpg" className="h-80 rounded-lg" />
-        <img src="/zayn.jpg" className="h-80 rounded-lg" />
+        <img src="./jb.jpg" className="h-80 rounded-lg" />
+        <img src="./taylor.jpg" className="h-80 rounded-lg" />
+        <img src="./camila.jpg" className="h-80 rounded-lg" />
+        <img src="./selena.jpg" className="h-80 rounded-lg" />
+        <img src="./zayn.jpg" className="h-80 rounded-lg" />
       </div>
     </div>
   );
